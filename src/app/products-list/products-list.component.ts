760fa5e18c6c5bd2ac75d59401d2280730e43dee
Dhibin <dhibin.ud@gmail.com>
2024-05-27 17:46:03 +0530

    Two way data binding

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'products-list',
   standalone: true,
-  imports: [],
+  imports: [SearchComponent],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css',
 })
---
+++ src/app/products-list/search/search.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent {
+  searchText: string = 'Womens watch';
+
+  updateSearchText(event: any) {
+    this.searchText = event.target.value;
+  }
+}
