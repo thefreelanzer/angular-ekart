8c274f3f78f1e71825af6ad4484f7058a06e2518
thefreelanzer <info@thefreelanzer.com>
2024-06-03 15:06:17 +0530

    1. Setvalue patchValue method 2. Resetting template driven form 3. Retrieving form data 4. Working with checkboxes

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -27,13 +27,83 @@ export class RegistrationFormComponent {
       display: 'Other',
     },
   ];
+
+  first_name: string = '';
+  last_name: string = '';
+  dob: string;
+  email: string;
+  username: string;
+  country: string;
+  city: string;
+  region: string;
+  postal: string;
+  submitted: boolean = false;
+  IsAgreed: boolean = false;
+
   @ViewChild('regform')
   formElements: NgForm;
   onSubmit() {
-    console.log(this.formElements);
-    console.log(this.formElements.value.first_name);
-    console.log(this.formElements.value.address.city);
+    // console.log(this.formElements);
+    // console.log(this.formElements.value.first_name);
+    // console.log(this.formElements.value.address.city);
+    this.country = this.formElements.value.address.country;
+    this.city = this.formElements.value.address.city;
+    this.region = this.formElements.value.address.region;
+    this.postal = this.formElements.value.address.postal_code;
+
+    this.submitted = true;
+    // this.formElements.reset();
+    // this.formElements.form.patchValue({
+    //   gender: 'male',
+    //   address: {
+    //     country: 'India',
+    //   },
+    // });
 
     // console.log(this.formElements.controls['first_name'].value);
+  }
+
+  generateUsername() {
+    let username = '';
+
+    if (this.first_name.length >= 3) {
+      username += this.first_name.slice(0, 3);
+    } else {
+      username += this.first_name;
+    }
+
+    if (this.last_name.length >= 3) {
+      username += this.last_name.slice(0, 3);
+    } else {
+      username += this.last_name;
+    }
+
+    let birthdate = new Date(this.dob);
+    username += birthdate.getFullYear();
+
+    this.username = username.toLowerCase();
+    // console.log(username);
+
+    // this.formElements.setValue({
+    //   first_name: this.formElements.value.first_name,
+    //   last_name: this.formElements.value.last_name,
+    //   email: this.formElements.value.email,
+    //   phone: this.formElements.value.phone,
+    //   dob: this.formElements.value.dob,
+    //   gender: this.formElements.value.gender,
+    //   uname: username,
+    //   address: {
+    //     address_line1: this.formElements.value.address.address_line1,
+    //     address_line2: this.formElements.value.address.address_line2,
+    //     country: this.formElements.value.address.country,
+    //     city: this.formElements.value.address.city,
+    //     region: this.formElements.value.address.region,
+    //     postal_code: this.formElements.value.address.postal_code,
+    //   },
+    // });
+
+    this.formElements.form.patchValue({
+      uname: this.username,
+    });
   }
 }
